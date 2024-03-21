93a377116539a777b2bba873932bb1a0cd32d8aa
Xenonx1 <160307185+Xenonx1@users.noreply.github.com>
2024-03-21 21:13:34 +0530

    Update and rename sample_info.py to Sinfo.py

---
+++ Sinfo.py
@@ -0,0 +1,24 @@
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 19654909
+API_HASH = '73f806effc9e93eca3fb5bb224a8465d'
+BOT_TOKEN = '7117210586:AAGPqyzTVjYihbikZtDBri4SZwJHjFk2bFY'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [,1909886542 'admin123', 98765432]
+CHANNELS = [-1002095842555]
+AUTH_USERS = [1909886542]
+AUTH_CHANNEL = -1002095842555
+
+# MongoDB information
+DATABASE_URI = "mongodb+srv://exos5:exos5@exos5.ns3h646.mongodb.net/"
+DATABASE_NAME = 'Telegram'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+
